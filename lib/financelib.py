c5d8211a47690bc35177018708d3ad75d10b086d
Andrei <potra.andrei@tutanota.com>
2025-03-29 17:26:00 +0100

    implemented personal assets history holdings in library

--- lib/financelib.py
+++ lib/financelib.py
@@ -1,10 +1,17 @@
 from pathlib import Path
 import pandas as pd
 import json
+import os
 
 from plotly.subplots import make_subplots
 from plotly import graph_objects as go
 from plotly import express as px
+
+# For market data web scraping
+import requests
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 
 class FinLoad:
     def load_init_holdings(path: Path, YEAR: int):
@@ -69,7 +76,7 @@ class FinLoad:
         else:
             print(f"{path} does not exist.")
             return None
-    
+
 
 class FinCalc:
     def calc_monthly_cashflow(df_year_cashflow):
@@ -100,7 +107,179 @@ class FinCalc:
         
         return df_expenses
 
+class FinInvestmentsGet:
+    def get_init_holdings_to_df(init_holdings, YEAR: int):
+        rows = list()
+        for asset_class in init_holdings['assets'].keys():
+            symbols = list(init_holdings['assets'][asset_class].keys())
+            for symbol in symbols:
+                new_row = pd.DataFrame(
+                    {
+                        'Date': [f"{YEAR}-01-01"],
+                        'Type': [asset_class],
+                        'Symbol': [symbol],
+                        'Qty': [ init_holdings['assets'][asset_class][symbol] ],
+                        'Category': ['Init'],
+                        'Subcategory': ['Holdings'],
+                        'Description': ['From previous year']
+                    }
+                )
+                new_row['Date'] = pd.to_datetime(new_row['Date'])
+                new_row.set_index('Date',inplace=True)
+                rows.append(new_row)
 
+        df_init_investments = pd.concat(rows)
+        return df_init_investments
+
+    # For each symbol of each asset class, initializes the date index series
+    # as dates from january till december with zero values, and then updates it
+    # with imported values from df_year_investments
+    def get_holdings_monthlyized(df_year_investments, YEAR: int):
+        complete_index = pd.date_range(start=f'{YEAR}-01-01', end=f'{YEAR}-12-31', freq='ME') # End of month
+        df_month_fill = pd.DataFrame(index=complete_index, data=0.0, columns=["Qty"], dtype='float64')
+
+        holdings_monthlyized = dict()
+        asset_classes = df_year_investments["Type"].unique()
+        for asset_class in asset_classes:
+            holdings_monthlyized_per_symbol = dict()
+            symbols = df_year_investments.loc[df_year_investments["Type"]==asset_class]["Symbol"].unique()
+            for symbol in symbols:
+                df_year_investments_query_asset_class = df_year_investments.loc[ df_year_investments.Type == asset_class ]
+                df_symbol_transactions = df_year_investments_query_asset_class.loc[ df_year_investments_query_asset_class.Symbol == symbol ]
+                
+                # Monthlyized quantities to end of month
+                df_month_invest = df_symbol_transactions.resample(rule='ME')['Qty'].sum().to_frame(name='Qty')
+                
+                temp_fill = df_month_fill.copy()
+                temp_fill.update(df_month_invest)
+                temp_fill['CumQty'] = temp_fill.values.cumsum()
+                holdings_monthlyized_per_symbol[symbol] = temp_fill
+            
+            holdings_monthlyized[asset_class] = holdings_monthlyized_per_symbol
+        
+        return holdings_monthlyized
+
+    # For each symbol of each asset class, load historical data
+    # with monthly resolution
+    def get_assets_monthlyized(holdings_monthlyized, path: Path, YEAR: int):
+        currency = 'EUR'
+        years_watchback = 5
+
+        assets_monthlyized = dict()
+        for asset_class in holdings_monthlyized.keys():
+            assets_per_class = dict()
+            for symbol in holdings_monthlyized[asset_class].keys():
+                maket_data_path = Path(f"{path}/{YEAR}/investments/exchange/{symbol}-{currency}.csv")
+                print(symbol)
+
+                if not os.path.exists(maket_data_path):
+                    if asset_class == "Cryptocurrencies":
+                        asset_history = FinFetch.fetch_crypto_data(symbol, currency, years_watchback)
+                    elif asset_class == "ETFs":
+                        asset_history = FinFetch.fetch_etf_data(symbol, currency, years_watchback)
+                    time.sleep(5) # sleep for preventing status resp 500 (ip addres based limitation)
+                    
+                    asset_history = asset_history.loc[f'{YEAR}-01-01':f'{YEAR}-12-31']
+                    asset_history.to_csv(maket_data_path)
+                    print(f"Data saved in local to {maket_data_path}")
+                else:
+                    asset_history = pd.read_csv(maket_data_path, index_col=0, parse_dates=True)
+                    print(f"{maket_data_path} already exists. Data Loaded from local.")
+                
+                asset_history["Returns"] = (asset_history["Close"] - asset_history.shift(1)["Close"] )/ asset_history["Close"]
+                assets_per_class[symbol] = asset_history
+            assets_monthlyized[asset_class] = assets_per_class
+            
+        return assets_monthlyized
+
+    # JOIN ASSETS AND HOLDINGS TO GET PERSONAL HOLDINGS IN EUR
+    #          A  B             C  D                                             A  B  C  D
+    # month1   x  y     month1  u  v      pd.concat([df1, df2], axis=1)  month1  x  y  u  v  
+    # month2   x  y     month2  u  v                ------>              month2  x  y  u  v
+    def get_assets_global(assets_monthlyized, holdings_monthlyized):
+        assets = dict()
+        for asset_class in assets_monthlyized.keys():
+            assets_symb = dict()
+            for symbol in assets_monthlyized[asset_class].keys():
+                df_joined = pd.concat([assets_monthlyized[asset_class][symbol], holdings_monthlyized[asset_class][symbol]], axis=1)
+                df_joined["Holdings"] = df_joined["Close"]*df_joined["CumQty"]
+
+                assets_symb[symbol] = df_joined
+            assets[asset_class] = assets_symb
+        
+        return assets
+
+
+
+class FinFetch:
+    def fetch_crypto_data(symbol, currency="EUR", years_watchback=3):
+        url = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}-{currency}?range={years_watchback}y&interval=1mo"
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0'} # Set the user agent to mimic a web browser, otherwise error 429 too many requests
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            
+            # Extract the relevant data
+            timestamps = data['chart']['result'][0]['timestamp']
+            open_prices = data['chart']['result'][0]['indicators']['quote'][0]['open']
+            close_prices = data['chart']['result'][0]['indicators']['quote'][0]['close']
+            
+            asset_history = pd.DataFrame({
+                'Date': pd.to_datetime(timestamps, unit='s').strftime('%Y-%m-%d'),
+                'Open': open_prices,
+                'Close': close_prices
+            })
+
+            asset_history['Open'] =  asset_history['Open'].round(2)
+            asset_history['Close'] = asset_history['Close'].round(2)
+            asset_history['Date'] = pd.to_datetime(asset_history['Date'])
+            asset_history.set_index('Date',inplace=True)
+
+            # Change dates with end of month instead of start for coherence
+            asset_history.index = asset_history.index + pd.offsets.MonthEnd(0)
+
+            return asset_history
+        else:
+            print(f"Error fetching data: {response.status_code}")
+            print(url)
+            return None
+
+    def fetch_etf_data(isin, currency="EUR", years_watchback=3):
+        today = datetime.now()
+        query_end_date = today.strftime('%Y-%m-%d')
+        query_start_date = ( today - relativedelta(years=years_watchback) ).strftime('%Y-%m-%d')
+
+        url = f"https://www.justetf.com/api/etfs/{isin}/performance-chart?locale=en&currency={currency}&valuesType=MARKET_VALUE&reduceData=true&includeDividends=false&features=DIVIDENDS&dateFrom={query_start_date}&dateTo={query_end_date}"
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'} # User agent to mimic a web browser, otherwise error 429 too many requests
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            
+            dates = list()
+            close_prices = list()
+            for row in data['series']:
+                dates.append(row['date'])
+                close_prices.append(row['value']['raw'])
+
+            j_asset_history = pd.DataFrame({
+                'Date': pd.to_datetime(dates).strftime('%Y-%m-%d'),
+                'Close': close_prices
+            })
+
+            j_asset_history['Close'] = j_asset_history['Close'].round(2)
+            j_asset_history['Date'] = pd.to_datetime(j_asset_history['Date'])
+            j_asset_history.set_index('Date',inplace=True)
+
+            # Sample from daily to monthly and take end of month for coherence
+            j_asset_history = j_asset_history.resample(rule='ME').last()
+
+            return j_asset_history
+        else:
+            print(f"Error fetching data: {response.status_code}")
+            return None
 
 class FinPlot:
     def plot_cashflow(df_cashflow):
